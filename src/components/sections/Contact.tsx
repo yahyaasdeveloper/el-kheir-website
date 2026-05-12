c736da6c7f5933fa0a5bde3813095c88ae2b3396
Yahya <yahyaasdeveloper@gmail.com>
2026-05-12 22:52:54 +0000

    update

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -138,7 +138,7 @@ export function Contact() {
         >
           {/* Using a placeholder iframe for Maps, ideally should be a real location */}
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10500.56930818274!2d2.3429312217730873!3d48.85590924962292!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis%2C%20France!5e0!3m2!1sfr!2sfr!4v1700000000000!5m2!1sfr!2sfr"
+            src="https://maps.google.com/maps?q=Nouakchott&t=&z=12&ie=UTF8&iwloc=&output=embed"
             width="100%"
             height="100%"
             style={{ border: 0 }}
